17d354a7d0a5076da19e9942a4beb2440694b28c
Atsushi <atsushi@moongift.jp>
2018-08-03 11:29:32 +0900

    Initial Commit

---
+++ NCMBTodoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCMBTodoSwift
+//
+//  Created by nakatsugawa on 2018/08/03.
+//  Copyright © 2018年 MOONGIFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
